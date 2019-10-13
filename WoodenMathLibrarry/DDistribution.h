1b3e227eec7c5f405af3ad8cb6261221f1dfec1d
mishok43 <devmishok@gmail.com>
2019-10-14 01:48:06 +0300

    DDistribution

--- WoodenMathLibrarry/DDistribution.h
+++ WoodenMathLibrarry/DDistribution.h
@@ -7,11 +7,15 @@
 WML_BEGIN
 
 
-template<typename AllocT>
+template<typename T>
 class DDistributionbPieceWise1D
 {
 public:
-	DDistributionbPieceWise1D(const float* f, int n):
+	DDistributionbPieceWise1D()
+	{
+	}
+
+	DDistributionbPieceWise1D(const T* f, int n):
 		func(f, f+n), cdf(n+1)
 	{
 		cdf[0] = 0;
@@ -33,17 +37,17 @@ public:
 		return func.size();
 	}
 
-	float pdf(uint32_t i)
+	T pdf(uint32_t i)
 	{
 		return func[i] / funcInt;
 	}
 
-	float pdfDiscrete(uint32_t i)
+	T pdfDiscrete(uint32_t i)
 	{
 		return func[i] / (funcInt*size());
 	}
 
-	float sampleContinuous(float u, float& off) const
+	T sampleContinuous(T u, int& off, T& pdf) const
 	{
 		int offset = findInterval(cdf.size(), [&](int index)
 		{
@@ -52,15 +56,17 @@ public:
 
 		off = offset;
 
-		float du = u - cdf[offset];
+		T du = u - cdf[offset];
 		if ((cdf[offset + 1] - cdf[offset]) > 0){
 			du /= (cdf[offset + 1] - cdf[offset]);
 		}
+
+		pdf = func[offset]/funcInt;
 		
 		return (offset + du) / size();
 	}
 
-	int sampleDiscrete(float u, float& off, float& tQuantile) const
+	int sampleDiscrete(T u, T& tQuantile) const
 	{
 		int offset = findInterval(cdf.size(),
 								  [&](int index)
@@ -73,9 +79,60 @@ public:
 	}
 
 
+	T funcInt;
+	std::vector<T> func, cdf;
+};
+
+template<typename T>
+class DDistributionbPieceWise2D
+{
+public:
+	DDistributionbPieceWise2D(){ }
+
+
+	DDistributionbPieceWise2D(float* func, int nu, int nv)
+	{
+		conditionalV.resize(nv);
+		for (int v = 0; v < nv; ++v)
+		{
+			conditionalV[v] = DDistributionbPieceWise1D(&func[nu*v], nu);
+		}
+
+
+		std::vector<float> marginalF;
+		marginalF.resize(nv);
+		for (int v = 0; v < nv; ++v)
+		{
+			marginalF[v] = conditionalV[v].funcInt;
+		}
+
+		marginalV = DDistributionbPieceWise1D(marginalF.data(), nv);
+	}
+
+
+	DPoint2f sampleContinuous(const DPoint2f &u, float &pdf) const
+	{
+		float pdfs[2];
+		int v;
+		int uo;
+		float d1 = marginalV.sampleContinuous(u[1], v, pdfs[1]);
+		float d0 = conditionalV[v].sampleContinuous(u[0], uo, pdfs[0]);
+		pdf = pdfs[0] * pdfs[1];
+		return DPoint2f(d0, d1);
+	}
+
+	float pdf(const DPoint2f &p) const
+	{
+		int iu = clamp(int(p[0] * conditionalV[0].size()),
+					   0, conditionalV.size() - 1);
+		int iv = clamp(int(p[1] * marginalV.size()),
+					   0, marginalV.size() - 1);
+		return conditionalV[iv].func[iu] / marginalV.funcInt;
+	}
+
 protected:
-	std::vector<float, AllocT<float>> func, cdf;
-	float funcInt;
+	std::vector<DDistributionbPieceWise1D<T>> conditionalV;
+	DDistributionbPieceWise1D<T> marginalV;
 };
 
 WML_END
